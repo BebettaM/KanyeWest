517880ab9e867a0e9c8d0c2ad99a82f1860c8577
Bebetta Motondo <claramotondo2@yahoo.fr>
2023-04-10 13:43:17 -0500

    Kanye West Quotes

---
+++ KanyeWest/Program.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace KanyeWest;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var client = new HttpClient();
+
+        var kanyeURL = "https://api.kanye.rest/";
+
+        var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+        var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+        Console.WriteLine($"Kanye: '{kanyeQuote}'");
+
+        RonQuote();
+
+    }
+
+    public static void RonQuote()
+    {
+        var client = new HttpClient();
+
+        var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+        var ronResponse = client.GetStringAsync(ronURL).Result;
+
+        var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+        Console.WriteLine($"Ron: '{ronQuote}'");
+    }
+}
+
